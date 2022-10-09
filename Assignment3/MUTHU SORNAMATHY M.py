6fa98371a0feb5df53a3c47e5333dff16156591f
MariAshwiniB6102 <113792614+MariAshwiniB6102@users.noreply.github.com>
2022-10-09 18:58:12 +0530

    Add files via upload

---
+++ Assignment3/MUTHU SORNAMATHY M.py
@@ -0,0 +1,32 @@
+#Python Program for Led Blinking using Raspberry Pi
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+GPIO.output(11,1)
+time.sleep(1)
+GPIO.output(11,0)
+time.sleep(1)
+GPIO.output(11,1)
+time.sleep(1)
+GPIO.output(11,0)
+time.sleep(1)
+GPIO.cleanup()
+
+
+
+#Python Program for Traffic Light using Raspberry Pi
+from gpiozero import LED
+import time
+red=LED(16)
+yellow=LED(18)
+green=LED(17)
+red.on()
+time.sleep(5)
+red.off()
+yellow.on()
+time.sleep(2)
+yellow.off()
+green.on()
+time.sleep(5)
+green.off()
